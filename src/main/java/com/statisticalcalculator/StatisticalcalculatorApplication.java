dddbc36f34c27184815ea0cb122eddcc1525e1c1
Max Mukimov <mukimomn@mail.uc.edu>
2019-10-03 21:27:42 -0400

    Initial commit os StatisticalCalculator

---
+++ src/main/java/com/statisticalcalculator/StatisticalcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.statisticalcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticalcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticalcalculatorApplication.class, args);
+	}
+
+}
